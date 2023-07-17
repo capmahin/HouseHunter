78c45d81dfd4874b8c619d8bf5b83ff601813544
Hawkeye <36890222+capmahin@users.noreply.github.com>
2023-07-17 15:49:09 +0600

    add mongodb

---
+++ config/db.js
@@ -0,0 +1,10 @@
+import mongoose from "mongoose";
+import colors from 'colors';
+
+const connectDB = async () =>{
+    try {
+        
+    } catch (error) {
+        console.log(`Error in Mongodb ${error}`.bgRed.white)
+    }
+}
--- server.js
+++ server.js
@@ -19,5 +19,5 @@ const PORT = process.env.PORT || 8080;
 
 //run listen
 app.listen(PORT,()=>{
-    console.log(`Server Running on ${PORT}`.bgCyan.white)
+    console.log(`Server Running on ${process.env.DEV_MODE} mode on port ${PORT}`.bgCyan.white)
 })
