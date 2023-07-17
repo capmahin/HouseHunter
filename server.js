e89589ed07d008f27ff6cf57c88b65a0c1b9e36d
Hawkeye <36890222+capmahin@users.noreply.github.com>
2023-07-17 14:53:50 +0600

    add port

---
+++ server.js
@@ -0,0 +1,19 @@
+import express from "express";
+import colors from "colors";
+
+//rest object
+const app = express();
+
+//rest api
+
+app.get("/",(req,res)=>{
+    res.send("<h1>Welcome to house hunter app</h1>")
+});
+
+//PORT
+const PORT = 8080;
+
+//run listen
+app.listen(PORT,()=>{
+    console.log(`Server Running on ${PORT}`.bgCyan.white)
+})
