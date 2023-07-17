8a3412e935e4945972b98dd8a409493d1f83c091
Hawkeye <36890222+capmahin@users.noreply.github.com>
2023-07-18 02:07:59 +0600

    add resigter

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
-
+import { Routes, Route } from "react-router-dom";
+import HomePage from "./pages/HomePage";
+import Pagenotfound from "./pages/Pagenotfound";
+import Register from "./pages/Auth/Register";
+import Login from "./pages/Auth/Login";
 function App() {
   return (
     <>
-      <h1>HouseHunter</h1>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="*" element={<Pagenotfound />} />
+      </Routes>
     </>
   );
 }
