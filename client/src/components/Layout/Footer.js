ae991429894bf9264426155d0d6279c08409c57b
Hawkeye <36890222+capmahin@users.noreply.github.com>
2023-07-18 01:49:16 +0600

    working on client site

---
+++ client/src/components/Layout/Footer.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { Link } from "react-router-dom";
+const Footer = () => {
+  return (
+    <div className="footer">
+      <h1 className="text-center">All Right Reserved &copy; House Hunter</h1>
+      
+    </div>
+  );
+};
+
+export default Footer;
