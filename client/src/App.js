01336d9e06d1647a7f10bda6ec9dad57b7c3927f
Hawkeye <36890222+capmahin@users.noreply.github.com>
2023-07-18 12:35:11 +0600

    add location hook in login

--- client/src/App.js
+++ client/src/App.js
@@ -3,11 +3,16 @@ import HomePage from "./pages/HomePage";
 import Pagenotfound from "./pages/Pagenotfound";
 import Register from "./pages/Auth/Register";
 import Login from "./pages/Auth/Login";
+import Dashboard from "./pages/user/Dashboard";
+import PrivateRoute from "./components/Routes/Private";
 function App() {
   return (
     <>
       <Routes>
         <Route path="/" element={<HomePage />} />
+        <Route path="/dashboard" element={<PrivateRoute />}>
+          <Route path="" element={<Dashboard />} />
+        </Route>
         <Route path="/register" element={<Register />} />
         <Route path="/login" element={<Login />} />
         <Route path="*" element={<Pagenotfound />} />
